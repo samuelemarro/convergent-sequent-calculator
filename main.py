dba5b80743446ed8608994296f12d74aa188104c
Samuele Marro <marrosamuele@gmail.com>
2022-04-28 16:20:52 +0200

    Cleanup.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
+import sys
 
 from typing import List
 from antlr4 import CommonTokenStream, ParseTreeWalker, StdinStream, InputStream, ParserRuleContext
@@ -52,87 +52,22 @@ class TreeConverter(SequentCalculusListener):
         else:
             self.consequents.append(atom)
 
-
-def traverse(tree, rule_names, indent = 0):
-    if tree.getText() == "<EOF>":
-        return
-    elif isinstance(tree, TerminalNodeImpl):
-        print("{0}TOKEN='{1}'".format("  " * indent, tree.getText()))
-    else:
-        print("{0}{1}".format("  " * indent, rule_names[tree.getRuleIndex()]))
-        for child in tree.children:
-            traverse(child, rule_names, indent + 1)   
-
-def parse_tree(tree, rule_names):
-    if tree.getText() == '<EOF>':
-        return None
-    elif isinstance(tree, TerminalNodeImpl):
-        return tree.getText()
-    else:
-        parsed_children = []
-        for child in tree.children:
-            parsed_child = parse_tree(child, rule_names)
-            parsed_children.append(parsed_child)
-
-        return {
-            'rule' : rule_names[tree.getRuleIndex()],
-            'children' : parsed_children
-        }
-
-def parse_tree_2(tree, rule_names):
-    if tree.getText() == '<EOF>':
-        return None
-    elif isinstance(tree, TerminalNodeImpl):
-        return tree.getText()
-    else:
-        parsed_children = []
-        for child in tree.children:
-            parsed_child = parse_tree(child, rule_names)
-            parsed_children.append(parsed_child)
-
-        return {
-            'rule' : rule_names[tree.getRuleIndex()],
-            'children' : parsed_children
-        }
-
 def main():
-    #_input = 'w:(A AND (B AND C)) =>w:A'
-    #_input = 'w:(A AND B) => w:(A OR B)'
-    #_input = 'w:(A OR (NOT B)) => p:(A OR (NOT B))' # not provable
-    #_input = 'w:BOT => p:A'
-    #_input = 'w:p =>w:p'
-    #_input = 'qRp => pRq'
-    #_input = 'wRw => wRw'
-    #_input = 'w:(A IMPLIES B), wRw => w:A'
-    _input = 'w:A, w:(A IMPLIES ((B AND E) AND (C AND D))) => w:B'
-
-    #print(_input)
+    _input = sys.argv[1]
 
     _input = parsing.preprocess(_input)
-    print(bcolors.UNDERLINE + _input + bcolors.ENDC)
 
     lexer = SequentCalculusLexer(InputStream(_input))
     stream = CommonTokenStream(lexer)
     parser = SequentCalculusParser(stream)
 
     tree = parser.sequent()
-
-    # traverse(tree, parser.ruleNames)
-
-    # print(parse_tree(tree, parser.ruleNames))
-
-    #traverse(tree, parser.ruleNames)
     printer = TreeConverter()
     walker = ParseTreeWalker()
     walker.walk(printer, tree)
 
     sequent = Sequent(printer.antecedents, printer.consequents)
-
-    
-    # print('Prop variables:', rule.root.prop_variables())
-    # print('Semantic variables:', rule.root.semantic_variables())
-    # print('Labels:', rule.root.labels())
-    # print(parsing.get_immediate_children(Formula(preprocess('(A AND (B AND C))')), parser.ruleNames))
+    print(bcolors.UNDERLINE + str(sequent) + bcolors.ENDC)
 
     proof = solver.solve(sequent, RULES, parser.ruleNames)
 

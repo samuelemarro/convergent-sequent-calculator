8cd59767aa0e22c7fe71e7f7c4007393edaf0816
Samuele Marro <marrosamuele@gmail.com>
2022-04-28 17:44:13 +0200

    Added cross-platform color support.

--- colors.py
+++ colors.py
@@ -1,10 +1,33 @@
+import platform
+import subprocess
+import sys
+
+import colorama
+
+colorama.init()
+
+def check_cmd():
+    return subprocess.check_output('(dir 2>&1 *`|echo CMD);&<# rem #>echo PowerShell', shell=True)
+
 class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+    if sys.platform == 'win32' and check_cmd().decode('utf-8').startswith('CMD') and platform.release() != '10':
+        # Disable colors
+        HEADER = ''
+        OKBLUE = ''
+        OKCYAN = ''
+        OKGREEN = ''
+        WARNING = ''
+        FAIL = ''
+        ENDC = ''
+        BOLD = ''
+        UNDERLINE = ''
+    else:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
